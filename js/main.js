2e933181075b0ce7c51a9d0800dda45d864c58e2
AhmedSlmn <adverts890@gmail.com>
2022-04-05 17:59:55 +0300

    Upload

--- js/main.js
+++ js/main.js
@@ -5,31 +5,19 @@ document.querySelector(".fa-bars").onclick = function () {
     document.querySelector("nav").classList.toggle("toggle-menu")
 }
 
-document.querySelector("header .search-box form button").onclick = function () {
-
-    if(document.querySelector('.loc-input input').value == ""){
-        
-        document.querySelector('.loc-input input').focus();
-        return false
-        
-    }else{
-         return true
-    }
-}
-
 window.onscroll = function () {
 
     // Show The Arrow-up If Window scrollY Is >= 700 
     if (window.scrollY >= 400) {
 
-        document.querySelector("nav .fa-arrow-up").style.display = "block"
+        document.querySelector(".fa-arrow-up").style.display = "block"
 
     } else {
-        document.querySelector("nav .fa-arrow-up").style.display = "none"
+        document.querySelector(".fa-arrow-up").style.display = "none"
     }
 }
 
-document.querySelector("nav .fa-arrow-up").onclick = function () {
+document.querySelector(".fa-arrow-up").onclick = function () {
 
     window.scrollTo({
         top: 0,
@@ -37,7 +25,8 @@ document.querySelector("nav .fa-arrow-up").onclick = function () {
     })
 }
 
-document.querySelectorAll("nav ul a").forEach(function (e) {
+
+document.querySelectorAll("nav a").forEach(function (e) {
 
     e.onclick = function () {
         document.querySelector("nav").classList.remove("toggle-menu")
